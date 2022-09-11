126c5338ad28144f388abb99c9a02bc5c0b9d1bb
BachelorTSW <bachelortsw@gmail.com>
2022-09-11 10:55:56 +0200

    Handle malicious requests without spamming logs

--- src/main/java/com/swl/mod/rplist/config/SecurityConfig.java
+++ src/main/java/com/swl/mod/rplist/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.swl.mod.rplist.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.HttpStatusRequestRejectedHandler;
+import org.springframework.security.web.firewall.RequestRejectedHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -14,6 +17,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .anyRequest()
                 .anonymous();
+    }
+
+    @Bean
+    public RequestRejectedHandler requestRejectedHandler() {
+        // sends an error response with a configurable status code (default is 400 BAD_REQUEST)
+        return new HttpStatusRequestRejectedHandler();
     }
 
 }
